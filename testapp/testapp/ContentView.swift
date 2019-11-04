0bce8154a89b0e90b0ea2b5204e4a77105e8ca61
qtvb1987 <qtvb1987@163.com>
2019-11-04 21:21:09 +0800

    mac first Commit

---
+++ testapp/testapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testapp
+//
+//  Created by 卿佳文 on 2019/11/4.
+//  Copyright © 2019 卿佳文. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
